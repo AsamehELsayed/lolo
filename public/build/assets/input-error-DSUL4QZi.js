d2a246f051e7f4582cf28dbb39d641be7206bf05
sameh <abdelrahman2003.12.12@gmail.com>
2026-04-11 16:27:32 +0200

    gamed

---
+++ public/build/assets/input-error-DSUL4QZi.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-CNpOSU9s.js";import{c as o}from"./button-EE5M1J-a.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};

8db7d466ab05deb8bf14eaef54bf91e22efdfca3
sameh <abdelrahman2003.12.12@gmail.com>
2026-04-18 09:51:10 +0200

    gamde

---
+++ public/build/assets/input-error-Bb6b-QY1.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-BB8NopNY.js";import{a as o}from"./button-Cf7S-va4.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
